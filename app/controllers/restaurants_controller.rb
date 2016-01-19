aa937b4abb9d9f0aad7900dca1a38f9fdc1c073c
hibreez <4ngus.whitehead@gmail.com>
2016-01-19 10:39:04 +0000

    Creates form for adding new restaurants and controller methods for getting and submitting the form and creating a new restaurant in the db. Tested.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,16 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
