cd0421af5c08381a2d175a32379ccab754aab59c
hibreez <4ngus.whitehead@gmail.com>
2016-01-19 10:50:38 +0000

    Adds show view and controller for showing individual restaurant pages and adds a description to resaurant model.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,11 +4,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def restaurant_params
