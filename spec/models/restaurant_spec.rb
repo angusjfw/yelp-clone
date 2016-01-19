ba3ba64aa88c3dd5b7a0ca60c500b4fad1cf6385
hibreez <4ngus.whitehead@gmail.com>
2016-01-19 12:34:52 +0000

    Validates length of restaurant name and displays errors on form.

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,3 +3,11 @@ require 'spec_helper'
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 end
+
+describe Restaurant, :type => :model do
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+end
