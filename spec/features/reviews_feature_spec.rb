e8057ef88979c599e456b7fe69e8325d84ff4197
hibreez <4ngus.whitehead@gmail.com>
2016-01-19 11:53:29 +0000

    Modifies models so that reviews are destroyed if parent restaurant deleted.

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,4 +13,15 @@ feature 'reviewing' do
 		expect(current_path).to eq '/restaurants'
 		expect(page).to have_content('so so')
 	end
+
+  scenario 'deletes a review if its parent restaurant deleted' do
+    visit '/restaurants'
+		click_link 'Review KFC'
+		fill_in "Thoughts", with: "so so"
+		select '3', from: 'Rating'
+		click_button 'Leave Review'
+    click_link 'Delete KFC'
+
+    expect(Review.all).to eq []
+  end
 end
