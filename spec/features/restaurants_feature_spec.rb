9c3bfede2f0589e7695f4b79fb9439415c044ade
hibreez <4ngus.whitehead@gmail.com>
2016-01-19 10:08:00 +0000

    Adds test for displaying a restuarant from the database and adds a model and migration for this.

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,19 @@ feature 'restaurants' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_link 'Add a restaurant'
-    end
-  end
+			expect(page).to have_link 'Add a restaurant'
+		end
+	end
+end
+
+context 'restaurants have been added' do
+	before do
+		Restaurant.create(name: 'KFC')
+	end
+
+	scenario 'display restaurants' do
+		visit '/restaurants'
+		expect(page).to have_content('KFC')
+		expect(page).not_to have_content('No restaurants yet')
+	end
 end
