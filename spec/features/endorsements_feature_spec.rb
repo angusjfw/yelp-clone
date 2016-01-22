1714448754b5d4faccebff8e17713ad50e723113
hibreez <4ngus.whitehead@gmail.com>
2016-01-22 11:47:28 +0000

    Endorsements updated using AJAX.

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -6,18 +6,9 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
     visit '/restaurants'
     click_link 'Endorse review'
     expect(page).to have_content('1 endorsement')
-  end
-
-  context 'review with multiple endorsements' do
-    scenario 'dispays the correct count of endorsements' do
-      visit '/restaurants'
-      click_link 'Endorse review'
-      click_link 'Endorse review'
-      expect(page).to have_content('2 endorsements')
-    end
   end
 end
